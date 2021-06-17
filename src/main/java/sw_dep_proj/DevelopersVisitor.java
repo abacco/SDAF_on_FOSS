5638d62808607ffd0d9d9a2947a720469492e3f5
Alex Bacco <bacco.a10@gmail.com>
2021-06-17 17:26:31 +0200

    update repodriller and fix to the CSV logic

--- src/main/java/sw_dep_proj/DevelopersVisitor.java
+++ src/main/java/sw_dep_proj/DevelopersVisitor.java
@@ -1,6 +1,5 @@
 package sw_dep_proj;
 
-
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.repodriller.domain.Commit;
@@ -8,7 +7,6 @@ import org.repodriller.persistence.PersistenceMechanism;
 import org.repodriller.scm.CommitVisitor;
 import org.repodriller.scm.RepositoryFile;
 import org.repodriller.scm.SCMRepository;
-
 
 import sw_dep_proj.beans.ClassBean;
 import sw_dep_proj.beans.PackageBean;
@@ -24,21 +22,98 @@ import java.util.Vector;
 // This class represents a RepoDriller Processor
 // Implement CommitVisitor interface. 
 // Inside of process(), we print the commit hash and the name of the developer.
-public class DevelopersVisitor implements CommitVisitor{
+public class DevelopersVisitor implements CommitVisitor {
 
     boolean fal = false;
-    // write all the useful info to gather data
+
     @Override
+    public void process(SCMRepository scmRepository, Commit commit, PersistenceMechanism writer) {
+        // We will need a parser to mine the structural properties of a class.
+        CodeParser codeParser = new CodeParser();
+        if(!fal){
+            writer.write("commit hash", "Class Name", "LOC", "LCOM", "CBO");
+        }
+        // We want to compute metrics for all classes of the repository.
+        List<RepositoryFile> files = scmRepository.getScm().files();
+        for (RepositoryFile file : files) { // So, for each file available in the repository,
+
+            if (file.getFullName().contains(".java")) { // we check if the file is a java file, i.e., we do not want to consider configuration files and others.
+
+                // Afterwards, we start parsing the file.
+                CompilationUnit parsed;
+                try {
+                    //System.out.println("ciao sono qui");
+                    // We exploit the parser capabilities to read a file and transform it in something usable for analysis.
+                    parsed = codeParser.createParser(FileUtility.readFile(file.getFile().getAbsolutePath()));
+                    // Just for your information, the following instruction means that we will
+                    // analyze only the main class in a file, i.e., we DO NOT consider inner classes.
+                    TypeDeclaration typeDeclaration = (TypeDeclaration) parsed.types().get(0);
+
+                    // While we could already use the TypeDeclaration to compute metrics, for the sake of
+                    // understandability we are going to create more easily comprehensible objects, i.e.,
+                    // those defined in the package 'beans'
+
+
+                    Vector<String> imports = new Vector<String>();
+                    for (Object importedResource : parsed.imports())
+                        imports.add(importedResource.toString());
+
+
+                    PackageBean packageBean = new PackageBean();
+                    //packageBean.setName(parsed.getPackage().getName().getFullyQualifiedName());
+
+
+                    ClassBean classBean = ClassParser.parse(typeDeclaration, packageBean.getName(), imports);
+                    classBean.setPathToClass(file.getFile().getAbsolutePath());
+
+                    // Once converted the files read in our beans, we can compute metrics in a super-easy way.
+                    // NB: The class 'CKMetrics' contains a number of other metrics!
+                    int LOC = CKMetrics.getLOC(classBean);
+                    int LCOM = CKMetrics.getLCOM2(classBean);
+                    int CBO = CKMetrics.getCBO(classBean);
+
+                    // Now, let's pretty-print our results.
+                    writer.write(
+                            commit.getHash(),
+                            /*classBean.getBelongingPackage() + "." + */classBean.getName(),
+                            LOC,
+                            LCOM,
+                            CBO
+                    );
+
+                } catch (IOException e) {
+                        e.printStackTrace();
+                }
+            }
+        }
+
+/*        @Override
+        public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
+
+        writer.write(
+                commit.getHash(),
+                commit.getCommitter().getName(),
+                commit.getAuthor(),
+                commit.getMsg()
+        );*/
+
+    }
+}
+
+
+
+    // write all the useful info to gather data
+    /*@Override
     public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
     // We will need a parser to mine the structural properties of a class.
         CodeParser codeParser = new CodeParser();
 
         // We want to compute metrics for all classes of the repository.
         List<RepositoryFile> files = repo.getScm().files();
-        if(!fal) {
+*//*        if(!fal) {
             writer.write("LOC", "LCOM", "CBO", "ELOC", "COH", "WMC", "MESSAGE", "DATE","FILENAME","CHANGE TYPE");
             fal = true;
-        }
+        }*//*
         for (RepositoryFile file: files) { // So, for each file available in the repository,
             if (file.getFullName().contains(".java")) { // we check if the file is a java file, i.e., we do not want to consider configuration files and others.
 
@@ -77,22 +152,22 @@ public class DevelopersVisitor implements CommitVisitor{
 
                     // Now, let's pretty-print our results.
                     writer.write(
-                            LOC,
+*//*                            LOC,
                             LCOM,
                             CBO,
                             ELOC,
                             COH,
-                            WMC,
+                            WMC,*//*
                             commit.getMsg(),
                             commit.getDate().getTime(),
                             commit.getModifications().get(0).getFileName(),
                             commit.getModifications().get(0).getType()
                     );
                 } catch (IOException | IndexOutOfBoundsException | ArithmeticException e) {
-                    System.out.println();
+                    e.printStackTrace();
                 }
             }
         }
-    }
-    
-}
+    }*/
+
+
--- src/main/java/sw_dep_proj/MyStudy.java
+++ src/main/java/sw_dep_proj/MyStudy.java
@@ -1,27 +1,16 @@
 package sw_dep_proj;
 
+
 import org.repodriller.RepoDriller;
 import org.repodriller.RepositoryMining;
-import org.repodriller.Study;
-import org.repodriller.filter.commit.OnlyInBranches;
-import org.repodriller.filter.commit.OnlyInMainBranch;
-import org.repodriller.filter.commit.OnlyModificationsWithFileTypes;
-import org.repodriller.filter.commit.OnlyNoMerge;
 import org.repodriller.filter.range.Commits;
 import org.repodriller.persistence.csv.CSVFile;
-import org.repodriller.scm.CollectConfiguration;
+import org.repodriller.scm.CommitVisitor;
 import org.repodriller.scm.GitRemoteRepository;
 import org.repodriller.scm.GitRepository;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.Calendar;
-
-
 // ref : https://github.com/mauricioaniche/repodriller/blob/master/manual/repodriller-2.0.0.md
-public class MyStudy implements Study{
+public class MyStudy implements org.repodriller.Study {
 
     // RepoDriller needs a Study. The interface is quite simple: a single execute() method:
     public static void main(String[] args) {
@@ -30,7 +19,7 @@ public class MyStudy implements Study{
     }
 
 
-    public void mineRepos(){
+/*    public void mineRepos(){
 
         Path azurePath = Paths.get("src/main/java/sw_dep_proj/tmpDirStudy/azure");
         boolean azureMined = Files.exists(azurePath);
@@ -45,9 +34,9 @@ public class MyStudy implements Study{
         boolean oracleMined = Files.exists(oraclePath);
 
         Path shopizerPath = Paths.get("src/main/java/sw_dep_proj/tmpDirStudy/shopizer");
-        boolean shopizerMined = Files.exists(shopizerPath);
+        boolean shopizerMined = Files.exists(shopizerPath);*/
 
-        if(!seleniumMined && !netflixMined && !azureMined && !oracleMined && !shopizerMined ){
+/*        if(!seleniumMined && !netflixMined && !azureMined && !oracleMined && !shopizerMined ){
             String seleniumRepo = "https://github.com/SeleniumHQ/selenium.git";
             buildRepo(seleniumRepo, "selenium");
             String netflixRepo = "https://github.com/Netflix/zuul.git";
@@ -58,47 +47,136 @@ public class MyStudy implements Study{
             buildRepo(oracleRepo, "oracle");
             String shopizerRepo = "https://github.com/shopizer-ecommerce/shopizer.git";
             buildRepo(shopizerRepo, "shopizer");
-        }
-    }
+        }*/
+
+/*        String shopizerRepo = "https://github.com/shopizer-ecommerce/shopizer.git";
+        //buildRepo(shopizerRepo, "shopizer");
+    }*/
+
     // configure here your study,
     // projects to analyze, metrics to be executed, and output files
-    @Override
+
     public void execute() {
 
-        mineRepos();
-        System.out.println("repo mined - retrieving commits");
-            retrieveRepoCommits("selenium", "seleniumCSV");
+        String realPath = "C:\\Users\\bacco\\OneDrive\\Desktop\\progetti uni\\shopizer";
+
+        new RepositoryMining()
+                .in(GitRepository.singleProject(realPath))
+                .through(Commits.all())
+                .process(new DevelopersVisitor(),
+                            new CSVFile("src/main/java/sw_dep_proj/csv/shopizer.csv"))
+                .mine();
+
+/*        new RepositoryMining().
+                in(GitRemoteRepository
+                        .hostedOn("https://github.com/mauricioaniche/repodriller.git")
+                        .inTempDir("src/main/java/sw_dep_proj/tmpDirStudy/shopizer-prova2")
+                        .asBareRepos()
+                        .buildAsSCMRepository())
+        .through(Commits.all())
+        .process(new DevelopersVisitor(), new CSVFile("src/main/java/sw_dep_proj/csv/devs.csv"))
+        .mine();*/
+
+
+/*        mineRepos();
+        System.out.println("repo mined - retrieving commits");*/
+/*            retrieveRepoCommits("selenium", "seleniumCSV");
             retrieveRepoCommits("netflix", "netflixCSV");
             retrieveRepoCommits("azure", "azureCSV");
-            retrieveRepoCommits("oracle", "oracleCSV");
-            retrieveRepoCommits("shopizer", "shopizerCSV");
+            retrieveRepoCommits("oracle", "oracleCSV");*/
+/*            retrieveRepoCommits("shopizer", "ssShopizerCSV.csv");*/
+        //proof2();
     }
 
-    public void buildRepo(String githubLink, String dir){
+/*    public void buildRepo(String githubLink, String dir){
         GitRemoteRepository.hostedOn(githubLink)
                 .inTempDir("src/main/java/sw_dep_proj/tmpDirStudy/" + dir)
-                .asBareRepos()
+                //.asBareRepos();
                 .buildAsSCMRepository();
     }
 
-    // from 2020 to current year
-    public void retrieveRepoCommits(String projectRepoOutput, String projectCsvOutput){
+    public void proof2(){
+        Calendar fromCal = Calendar.getInstance();
+        fromCal.set(2000, 1,1);
+
+        Calendar toCal = Calendar.getInstance();
+        toCal.set(2020, 1, 1);*/
+
+/*        new RepositoryMining()
+                .in(GitRepository.singleProject("C:\\Users\\bacco\\OneDrive\\Desktop\\homework sw_dep\\jpacman-framework")) // let's test this project
+                //.through(Commits.all())
+                .through(Commits.betweenDates(fromCal, toCal))
+                .process(new DevelopersVisitor(), new CSVFile("src/main/java/sw_dep_proj/csv/proof2.csv")) // output folder
+                .filters(
+                        new OnlyModificationsWithFileTypes(Collections.singletonList(".java"))
+                        //new OnlyInBranches(Arrays.asList("master"))
+                        //new OnlyNoMerge(),
+                        //new OnlyInMainBranch()
+                )
+                .mine();*/
+    }
+
+    // funziona
+/*    public void proof(){
         Calendar fromCal = Calendar.getInstance();
         fromCal.set(2020, 1,1);
 
         Calendar toCal = Calendar.getInstance();
-        toCal.set(2021, 1, 1);
-
+        toCal.set(2021, 1, 1);*/
+/*
         new RepositoryMining()
-                .in(GitRepository.singleProject("C:\\Users\\bacco\\OneDrive\\Desktop\\progetti uni\\SDAF_on_FOSS\\src\\main\\java\\sw_dep_proj\\tmpDirStudy\\" + projectRepoOutput)) // let's test this project
-                .through(Commits.betweenDates(fromCal, toCal))
+                .in(GitRepository.singleProject("C:\\Users\\bacco\\OneDrive\\Desktop\\homework sw_dep\\jpacman-framework")) // let's test this project
+                .through(Commits.all())
+                //.through(Commits.betweenDates(fromCal, toCal))
+                .process((CommitVisitor) new DevelopersVisitor(), new CSVFile("src/main/java/sw_dep_proj/csv/proof.csv")) // output folder
                 .filters(
-                        new OnlyModificationsWithFileTypes(Arrays.asList(".java")),
-                        new OnlyInBranches(Arrays.asList("master")),
-                        new OnlyNoMerge(),
-                        new OnlyInMainBranch()
+                        new OnlyModificationsWithFileTypes(Collections.singletonList(".java"))
+                        //new OnlyInBranches(Arrays.asList("master"))
+                        //new OnlyNoMerge(),
+                        //new OnlyInMainBranch()
+                )
+                .mine();*/
+/*    }*/
+
+    // from 2020 to current year
+/*    public void retrieveRepoCommits(String projectRepoOutput, String projectCsvOutput){
+        Calendar fromCal = Calendar.getInstance();
+        fromCal.set(2000, 1,1);
+
+        Calendar toCal = Calendar.getInstance();
+        toCal.set(2021, 1, 1);*/
+
+/*        new RepositoryMining()
+                .in(GitRepository.singleProject("C:\\Users\\bacco\\OneDrive\\Desktop\\progetti uni\\shopizer")) // let's test this project
+                .through(Commits.all())
+                //.through(Commits.betweenDates(fromCal, toCal))
+                .filters(
+                        new OnlyModificationsWithFileTypes(Collections.singletonList(".java"))
+                        //new OnlyInBranches(Arrays.asList("master"))
+                        //new OnlyNoMerge(),
+                        //new OnlyInMainBranch()
                 )
                 .process(new DevelopersVisitor(), new CSVFile("src/main/java/sw_dep_proj/csv/" + projectCsvOutput)) // output folder
-                .mine();
-    }
-}
+                .mine();*/
+
+
+/*        new RepositoryMining()
+                .in(GitRepository.singleProject("C:\\Users\\bacco\\OneDrive\\Desktop\\progetti uni\\SDAF_on_FOSS\\src\\main\\java\\sw_dep_proj\\tmpDirStudy\\" + projectRepoOutput)) // let's test this project
+                .through(Commits.betweenDates(fromCal, toCal))
+*//*                .filters(
+                        new OnlyModificationsWithFileTypes(Arrays.asList(".java"))
+                        //new OnlyInBranches(Arrays.asList("master"))
+                        //new OnlyNoMerge(),
+                        //new OnlyInMainBranch()
+                )*//*
+                .process(new DevelopersVisitor(), new CSVFile("src/main/java/sw_dep_proj/csv/" + projectCsvOutput)) // output folder
+                .mine();*/
+
+/*        new RepositoryMining().in(GitRemoteRepository.singleProject("https://github.com/shopizer-ecommerce/shopizer.git"))
+                .through(Commits.all())
+                //.through(Commits.betweenDates(fromCal, toCal))
+                .filters(new OnlyModificationsWithFileTypes(Arrays.asList(".java")))
+                .process(new DevelopersVisitor(), new CSVFile("src/main/java/sw_dep_proj/csv/" + projectCsvOutput)) // output folder
+                .mine();*/
+/*    }
+}*/
